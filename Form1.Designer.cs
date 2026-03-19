9b7d19fa3546ff363cf893451380438914d0c0e2
kdy0618 <kdy0618@suwon.ac.kr>
2026-03-19 10:07:25 +0900

    과제1 코딩과 테스트 완료 ListBox를 중앙에 TextBox를ListBox 아래에 배치 전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lbl_title = new Label();
+            txt_input = new TextBox();
+            lst_messages = new ListBox();
+            btn_send = new Button();
+            SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            lbl_title.AutoSize = true;
+            lbl_title.Font = new Font("맑은 고딕", 35F, FontStyle.Bold);
+            lbl_title.ForeColor = Color.Blue;
+            lbl_title.Location = new Point(26, 24);
+            lbl_title.Name = "lbl_title";
+            lbl_title.Size = new Size(784, 125);
+            lbl_title.TabIndex = 0;
+            lbl_title.Text = "Echo Messenger";
+            // 
+            // txt_input
+            // 
+            txt_input.Font = new Font("맑은 고딕", 19.875F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txt_input.Location = new Point(44, 636);
+            txt_input.Name = "txt_input";
+            txt_input.Size = new Size(983, 78);
+            txt_input.TabIndex = 1;
+            // 
+            // lst_messages
+            // 
+            lst_messages.FormattingEnabled = true;
+            lst_messages.Location = new Point(44, 170);
+            lst_messages.Name = "lst_messages";
+            lst_messages.Size = new Size(1208, 420);
+            lst_messages.TabIndex = 2;
+            // 
+            // btn_send
+            // 
+            btn_send.BackColor = Color.FromArgb(128, 255, 128);
+            btn_send.Font = new Font("맑은 고딕", 20F);
+            btn_send.ForeColor = SystemColors.ControlText;
+            btn_send.Location = new Point(1051, 613);
+            btn_send.Name = "btn_send";
+            btn_send.Size = new Size(181, 121);
+            btn_send.TabIndex = 3;
+            btn_send.Text = "전송";
+            btn_send.UseVisualStyleBackColor = false;
+            btn_send.Click += btn_send_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1542, 846);
+            Controls.Add(btn_send);
+            Controls.Add(lst_messages);
+            Controls.Add(txt_input);
+            Controls.Add(lbl_title);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl_title;
+        private TextBox txt_input;
+        private ListBox lst_messages;
+        private Button btn_send;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,24 @@ namespace EchoMessenger
 {
     public partial class Form1 : Form
     {
+        public TextBox txtInput;
+        public ListBox lstMessages;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            // 텍스트 박스와 리스트 박스 가져오기
+            txtInput = txt_input;
+            lstMessages = lst_messages;
+            // 텍스트 박스에서 입력한 메시지 가져오기
+            String typed_msg = txtInput.Text;
+            // 가져온 메시지를 리스트 박스에 추가
+            lst_messages.Items.Add(typed_msg);
+            // 텍스트 박스 초기화
+            txtInput.Text = "";
         }
     }
 }
